f7f3f295a035cacd26cbbdf7a89dd5a7642d6d71
Adrian Khor <adriank96@outlook.com>
2022-08-28 21:34:47 +0900

    initial commit

--- WebAppExecuteClient/Program.cs
+++ WebAppExecuteClient/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Measure the time needed for loop to complete executing
+DateTimeOffset start = DateTimeOffset.Now;
+for (int i = 0; i < 1_000_000; i++)
+{
+    // Sleep for 10ms
+    // Thread.Sleep(10);
+}
+DateTimeOffset now = DateTimeOffset.Now;
+
+// Prepare the contents to be saved to desktop
+string message = $"Loop took {now.Subtract(start).Milliseconds} ms to complete";
+string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+    "appOutput.txt");
+
+// Save contents to a new file in desktop
+try
+{
+    File.WriteAllText(outputPath, message);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Couldn't save output to file. Error: {e}");
+}
